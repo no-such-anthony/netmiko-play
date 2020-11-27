c0a8efd16daf0dc038fd58ff1f6ddd35638ceb09
no-such-anthony <38369659+no-such-anthony@users.noreply.github.com>
2020-11-28 08:21:37 +1000

    Update netup.py

--- netup/netup.py
+++ netup/netup.py
@@ -4,32 +4,32 @@ import traceback
 
 def net_wrapper(net_task, a_device):
 
-    a_result = {}
-    a_result['host'] = a_device['host']
+    result = {}
+    result['host'] = a_device['host']
 
     try:
-        a_result['result'] = net_task(a_device)
+        result['result'] = net_task(a_device)
 
     except Exception as e:
-        a_result['exception'] = e
-        a_result['traceback'] = traceback.format_exc()
+        result['exception'] = e
+        result['traceback'] = traceback.format_exc()
 
-    return a_result
+    return result
 
 
 def net_runner(net_task, net_devices):
-    results = []  
+    net_results = []  
     with ThreadPoolExecutor(max_workers=4) as pool:
         futures = {pool.submit(net_wrapper, net_task, a_device): a_device for a_device in net_devices.values()}
         for future in as_completed(futures):
-            a_result = future.result() 
-            results.append(a_result) 
-            if 'exception' in a_result:
-                print(f"{a_result['host']} - {a_result['traceback']}")
+            result = future.result() 
+            net_results.append(result) 
+            if 'exception' in result:
+                print(f"{result['host']} - {result['traceback']}")
             else:
-                print(f"{a_result['host']} - complete")
+                print(f"{result['host']} - complete")
 
-    return results
+    return net_results
 
 
 def get_devices():
