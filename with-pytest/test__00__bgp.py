d665c2c29cbfbb0221e921daf84fad28b606a9e4
no-such-anthony <38369659+no-such-anthony@users.noreply.github.com>
2021-05-03 15:44:18 +1000

    removed __wrapped__ and move fixture functionality into pytest_generate_tests

--- with-pytest/test__00__bgp.py
+++ with-pytest/test__00__bgp.py
@@ -2,28 +2,25 @@ import pytest
 from genie.utils import Dq
 
 
-@pytest.fixture(scope='module')
-def bgp_ipv4_unicast_summary(device):
-    return device.remote_conn.send_command('show bgp ipv4 unicast summary', use_genie=True)
-
-
 def name_test(item):
     return f"bgp neighbor {item}"
 
 
 def pytest_generate_tests(metafunc):
-    #Not sure how best to do this, using wrapped as suggested at
-    #https://github.com/pytest-dev/pytest/issues/6374
 
-    b = bgp_ipv4_unicast_summary.__wrapped__(metafunc.config._net_device)
+    device = metafunc.config._net_device
+    output = device.remote_conn.send_command('show bgp ipv4 unicast summary', use_genie=True)
+    device.data['bgp_ipv4_unicast_summary'] = output
 
     metafunc.parametrize('neighbor',
-                         Dq(b).get_values('neighbor'),
+                         Dq(output).get_values('neighbor'),
                          ids=name_test
                          )
 
 
-def test_bgp_summary(device,bgp_ipv4_unicast_summary,neighbor):
+def test_bgp_summary(device,neighbor):
+    
+    bgp_ipv4_unicast_summary = device.data['bgp_ipv4_unicast_summary']
     state_pfxrcd = Dq(bgp_ipv4_unicast_summary).contains(neighbor).get_values('state_pfxrcd')[0]
 
     if state_pfxrcd in ['Idle','Active']:
